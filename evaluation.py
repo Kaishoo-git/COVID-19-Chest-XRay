0fbb5a332f47b0a577b172c3daa83086b4eb55fd
kaishoo <e0957315@u.nus.edu>
2024-11-06 23:58:44 +0800

    tensorboard implementation 11.6

--- evaluation.py
+++ evaluation.py
@@ -3,18 +3,19 @@ from torch.optim import lr_scheduler
 import torch.nn as nn
 import time
 import copy
+from torch.utils.tensorboard import SummaryWriter
 
-# Need to Re consider validation and stepping of gradients
-def train_model(model, train_loader, validation_loader, epochs = 5, learning_rate = 0.1, grad_criterion = nn.BCEWithLogitsLoss()):
+# Creates a tensorboard scalar at the same time
+def train_model(model, train_loader, validation_loader, writer, epochs = 5, learning_rate = 0.1, grad_criterion = nn.BCEWithLogitsLoss()):
     optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
     scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode = 'min', factor = 0.8, patience = 1)
-    best_model, best_loss = copy.deepcopy(model.state_dict()), 9999.9
+    best_model, best_loss, best_acc = copy.deepcopy(model.state_dict()), 9999.9, 0.0
     starttime = time.time()
     
     print("Training model...")
 
     for epoch in range(epochs):
-        epoch_loss, t_loss, v_loss = 0.0, [], []
+        epoch_loss, running_correct, t_loss, v_loss, t_acc, v_acc = 0.0, 0.0, [], [], [], []
 
         for phase in ['train', 'val']:
             if phase == 'train':
@@ -23,45 +24,55 @@ def train_model(model, train_loader, validation_loader, epochs = 5, learning_rat
                 model.eval()  
         
             target_loader = train_loader if phase == 'train' else validation_loader
+            # n_steps = len(target_loader)
         
             for i, (images, labels) in enumerate(target_loader):
                 with torch.set_grad_enabled(phase == 'train'):
                     # forward pass
                     y_preds = model(images)
+                    pred_labels = (torch.sigmoid(y_preds) >= 0.5)
                     loss = grad_criterion(y_preds, labels)
                     epoch_loss += loss.item()
+                    running_correct += (pred_labels == labels).sum().item()
 
                     if phase == 'train':
                         # backward prop
                         optimizer.zero_grad()
                         loss.backward()
-                        optimizer.step()    
+                        optimizer.step()
 
             if phase == 'train':
-                avg_loss = epoch_loss / i
+                avg_loss, avg_correct = epoch_loss / i, running_correct / i
                 t_loss.append(avg_loss)
+                t_acc.append(avg_correct)
                 # scheduler step
                 scheduler.step(epoch_loss)    
 
             if phase == 'val':
-                avg_loss = epoch_loss / i
+                avg_loss, avg_correct = epoch_loss / i, running_correct / i
                 v_loss.append(avg_loss)
+                v_acc.append(avg_correct)
                 if avg_loss < best_loss:
-                    best_loss = avg_loss
+                    best_loss, best_acc = avg_loss, avg_correct
                     best_model = copy.deepcopy(model.state_dict())
-
-        print(f"Epoch [{epoch + 1}/{epochs}] |Training Loss: {t_loss[-1]:.4f} | Validation Loss: {v_loss[-1]:.4f}")  
+            
+        print(f"Epoch [{epoch + 1}/{epochs}] |Training Loss: {t_loss[-1]:.4f} | Validation Loss: {v_loss[-1]:.4f}")
+        writer.add_scalar('Loss/training', t_loss[-1], epoch)
+        writer.add_scalar('Loss/validation', v_loss[-1], epoch)
+        writer.add_scalar('Accuracy/training', t_acc[-1], epoch)
+        writer.add_scalar('Accuracy/validation', v_acc[-1], epoch)
+    writer.close()
 
     
     elapsedtime = time.time() - starttime
     mins, sec = elapsedtime//60, elapsedtime%60
     print(f"Training completed in {mins:.0f}mins {sec:.0f}s")
-    print(f"Best Loss: {best_loss}")
+    print(f"Best Loss: {best_loss:.4f} | Best Accuracy: {(best_acc*100):.0f}")
 
     model.load_state_dict(best_model)
     return best_model
     
-def retrain_model(model, train_loader, validation_loader, epochs = 5, learning_rate = 0.1, grad_criterion = nn.BCEWithLogitsLoss()):
+def retrain_model(model, train_loader, validation_loader, writer, epochs = 5, learning_rate = 0.1, grad_criterion = nn.BCEWithLogitsLoss()):
     with torch.no_grad():
         model.conv1.weight = nn.Parameter(model.conv1.weight.mean(dim=1, keepdim=True))
     for params in model.parameters():
@@ -71,28 +82,32 @@ def retrain_model(model, train_loader, validation_loader, epochs = 5, learning_r
     best_model = train_model(model, train_loader, validation_loader, epochs, learning_rate, grad_criterion)
     return best_model
 
-def evaluate_model(model, test_loader, criteria):
+# Creates a tensorboard AUC curve at the same time
+def get_metrics(model, test_loader, writer):
+    g_lab, preds = [], []
     with torch.no_grad():
         tp = tn = fp = fn = t = f = 0
         for j, (images, labels) in enumerate(test_loader):
             outputs = model(images)
+            probs = torch.sigmoid(outputs)
             y_preds = (torch.sigmoid(outputs) >= 0.5).float()
             n = len(y_preds)
             for i in range(n):
-                pred_val, label_val = y_preds[i].item(), labels[i].item() 
+                pred_val, label_val = y_preds[i].item(), labels[i].item()
                 tp += (pred_val == label_val == 1)
                 tn += (pred_val == label_val == 0)
                 fp += (pred_val == 1 and label_val == 0)
                 fn += (pred_val == 0 and label_val == 1)
                 f += (pred_val == 0)
                 t += (pred_val == 1)
-    j += 1
-    match criteria:
-        case 'acc':
-            return ((tp + tn) / j), t, f, j
-        case 'rec':
-            return tp / (tp + fn), t, f, j
-        case 'prec':
-            return tp / (tp + fp), t, f, j
-        case 'f1':
-            return (2 * tp) / (2 * tp + fp + fn), t, f, j
+
+            preds.append(probs)
+            g_lab.append(labels)
+
+        j += 1
+        preds = torch.stack(preds)
+        labels = torch.stack(labels)
+        writer.add_pr_curve('covid19', g_lab, preds, 0)
+        writer.close()
+    recall, prec = tp / (tp + fn), tp / (tp + fp)
+    return recall, prec, t, f, j
--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from dataLoader import *
 from models import *
 from evaluation import *
 import time
-from torch.utils.tensorboard import SummaryWriter
+import sys
 
-BATCH_SIZE = 16
+from torch.utils.tensorboard import SummaryWriter
+import torchvision
+
+BATCH_SIZE = 8
+
 def main():
     writer = SummaryWriter("runs/COVID19")
 
@@ -14,13 +18,12 @@ def main():
     # Data Loader
     print("Loading Data...")
     augmented_dataset = Covid19DataSet('train', transform = 'augment')
-    print(f"Augmented Train of length {len(augmented_dataset)} Loaded")
     validation_dataset = Covid19DataSet('val', transform = 'vanilla')
-    print(f"Validation of length {len(validation_dataset)} Loaded")
     test_dataset = Covid19DataSet('test', transform = 'vanilla')
     data_time = time.time() - data_since
     data_mins, data_sec = data_time//60, data_time % 50
-    print (f"Data Loaded in {data_mins:.0f}mins {data_sec:.0f}s")
+    print(f"Train size: {len(augmented_dataset)} | Validation size: {len(validation_dataset)} | Test size {len(test_dataset)}")
+    print (f"Time taken: {data_mins:.0f}mins {data_sec:.0f}s")
 
     print("Creating DataLoaders")
     loader_since = time.time()
@@ -29,27 +32,41 @@ def main():
     test_loader = DataLoader(dataset = test_dataset, batch_size = BATCH_SIZE, shuffle = False, num_workers = 4)
     loader_time = time.time() - loader_since
     loader_mins, loader_sec = loader_time//60, loader_time % 50
-    print (f"Data Loaded in {loader_mins:.0f}mins {loader_sec:.0f}s")
+    print(f"Train size: {len(train_loader)} | Validation size: {len(validation_loader)} | Test size {len(test_loader)}")
+    print (f"Time taken: {data_mins:.0f}mins {data_sec:.0f}s")
 
-    gen = iter(train_loader)
-    images, labels = next(gen)
-    # output = globpool_model(features)
-    print(images)
-    # print("Creating Models")
-    # model_22 = ConvNetGlobPooling()
-    # print("Models Created")
+    ### TensorBoard add sample Images
+    vanilla_dataset = Covid19DataSet('train', transform = 'vanilla')
+    vanilla_loader = DataLoader(dataset = vanilla_dataset, batch_size = BATCH_SIZE, shuffle = False, num_workers = 4)
+    gen = iter(vanilla_loader)
+    sample_img, sample_labels = next(gen)
+    img_grid = torchvision.utils.make_grid(sample_img)
+    writer.add_image('xray_images', img_grid)
+    ###
 
-    # # Model Training 
-    # print("Training global pooling model with augmented dataset")
-    # train_model(model_22, train_loader, validation_loader)
+    # Model 
+    model = models.resnet18(weights = 'DEFAULT')
+    with torch.no_grad():
+        model.conv1.weight = nn.Parameter(model.conv1.weight.mean(dim=1, keepdim=True))
+    for params in model.parameters():
+        params.requires_grad = False
+    num_features = model.fc.in_features
+    model.fc = nn.Linear(num_features, 1)
 
-    # # Model evaluation
+    # TensorBoard add Graph for model architecture
+    writer.add_graph(model, sample_img)
+    writer.close()
 
-    # metric_22_prec = evaluate_model(model_22, test_loader, 'prec') * 100
-    # metric_22_rec = evaluate_model(model_22, test_loader, 'rec') * 100
-    # print(f"model 2.2: Precision = {metric_22_prec:.0f}% | Recall = {metric_22_rec:.0f}%")
+    # Model Training, creates a tensorboard scalar 
+    resnet_trained = train_model(model, train_loader, validation_loader, writer)
+
+    # Model evaluation, creates a tensorboard AUC curve
+    recall, prec, t, f, n = get_metrics(resnet_trained, test_loader, writer)
+    print(f"Total Entries: {n} | Positive Preds: {t} | Negative Preds: {f}")
+    print(f"Fine-tuned: Precision = {prec:.0f}% | Recall = {recall:.0f}%")
     
     # Model visualisation with GradCam
+
 
 if __name__ == "__main__":
     main()
